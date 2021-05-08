d0fbe1913da87e10c989444cffb91e55719f005c
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-05-08 01:19:13 +0100

    Polished landing page UI

--- components/buttons/link-button.js
+++ components/buttons/link-button.js
@@ -1,18 +1,13 @@
-const LinkButton = ({name, url, secondary}) => {
-    return (
-        <div className={secondary? "mt-3 sm:mt-0 sm:ml-3": "rounded-full"}>
-            <a
-                href={url}
-                className={
-                    secondary
-                        ? "w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-white bg-gray-700 hover:text-indigo-200 hover:shadow-lg md:py-4 md:text-lg md:px-10"
-                        : "w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-full text-gray-700 bg-white hover:text-indigo-700 hover:shadow-lg md:py-4 md:text-lg md:px-10"
-                }
-            >
-                {name}
-            </a>
-        </div>
-    )
-}
+import Link from "next/link";
+
+const LinkButton = ({name, url, style}) => (
+    <Link href={url}>
+        <a
+            className={style}
+        >
+            {name}
+        </a>
+    </Link>
+)
 
 export default LinkButton
