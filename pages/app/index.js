30f821b4ccb9c4b8ddca8304656bf35c6f03a2e1
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-05-10 13:44:51 +0100

    Minor App page UI fixes

--- pages/app/index.js
+++ pages/app/index.js
@@ -2,7 +2,7 @@ import {ServersList} from "../../components/server";
 
 function App() {
     return (
-        <div className={"relative overflow-x-scroll scroll-hidden h-full flex scroll-snap-x"}>
+        <div className={"relative bg-gray-700 overflow-x-scroll scroll-hidden h-full flex scroll-snap-x"}>
             <section className={"scroll-snap-start flex-shrink-0 w-84"}>
                 <div className={"fixed left-0 flex h-full w-full"}>
                     <nav className={"w-20 bg-gray-900 h-full"}>
@@ -11,10 +11,10 @@ function App() {
                     <div className={"w-64 bg-gray-800 h-full"}/>
                 </div>
             </section>
-            <section className={"scroll-snap-start z-10 w-screen bg-gray-700 flex-shrink-0 md:flex-1"}>
+            <section className={"scroll-snap-start z-20 w-screen bg-gray-700 flex-shrink-0 lg:flex-1"}>
 
             </section>
-            <section className={"scroll-snap-start flex-shrink-0 w-72 bg-gray-800"}>
+            <section className={"scroll-snap-start z-10 flex-shrink-0 w-72 bg-gray-800"}>
 
             </section>
         </div>
