ff9da8dcfd21a0f84aead903267ad1fa7a48e447
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-05-24 22:40:31 +0100

    Updated channelListView to still show open channel when the disclosure is closed

--- components/channelsList/channelsListViews.js
+++ components/channelsList/channelsListViews.js
@@ -29,22 +29,37 @@ function ChannelsListLoaded({channels}) {
             {
                 categories && categories.map((category) =>
                     <Disclosure key={category.category} as={"div"} className={"mb-6 text-gray-400"}>
-                        <Disclosure.Button className={"flex w-full mb-2 h-4 items-center focus:outline-none"}>
-                            <ChevronDownIcon className={"mr-1 h-4 w-4 stroke-current "}/>
-                            <div className={"text-gray-400 uppercase font-medium text-xs"}>{category.category}</div>
-                        </Disclosure.Button>
-                        <Disclosure.Panel>
-                            {category.channels.map((channel) => (
-                                <div
-                                    key={channel.id}
-                                    className={`${(currentChannel === channel) && "text-white font-medium bg-gray-700"} flex items-center mb-1 pl-4 h-10 rounded-md cursor-pointer w-full hover:bg-gray-700 hover:text-white`}
-                                    onClick={() => setCurrentChannel(channel)}
-                                >
-                                    <HashtagIcon className={"w-5 h-5 mr-1"}/>
-                                    {channel.name}
-                                </div>
-                            ))}
-                        </Disclosure.Panel>
+                        {({open}) => (
+                            <>
+                                <Disclosure.Button className={"flex w-full mb-2 h-4 items-center focus:outline-none"}>
+                                    <ChevronDownIcon
+                                        className={`transition-transform mr-1 h-3 w-3 stroke-current ${!open && "transform -rotate-90"}`}/>
+                                    <div
+                                        className={"text-gray-400 uppercase font-medium text-xs"}>{category.category}</div>
+                                </Disclosure.Button>
+                                <Disclosure.Panel>
+                                    {category.channels.map((channel) => (
+                                        <div
+                                            key={channel.id}
+                                            className={`${(currentChannel === channel) && "text-white font-medium bg-gray-700"} flex items-center mb-1 pl-4 h-10 rounded-md cursor-pointer w-full hover:bg-gray-700 hover:text-white`}
+                                            onClick={() => setCurrentChannel(channel)}
+                                        >
+                                            <HashtagIcon className={"w-5 h-5 mr-1"}/>
+                                            {channel.name}
+                                        </div>
+                                    ))}
+                                </Disclosure.Panel>
+                                {
+                                    (!open && currentChannel && category.category === currentChannel.category) &&
+                                    <div
+                                        className={`text-white font-medium bg-gray-700 flex items-center mb-1 pl-4 h-10 rounded-md cursor-pointer w-full hover:bg-gray-700 hover:text-white`}
+                                    >
+                                        <HashtagIcon className={"w-5 h-5 mr-1"}/>
+                                        {currentChannel.name}
+                                    </div>
+                                }
+                            </>
+                        )}
                     </Disclosure>
                 )
             }
